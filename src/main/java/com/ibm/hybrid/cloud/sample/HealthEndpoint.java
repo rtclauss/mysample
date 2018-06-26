e37ef2f49d1167e88e5b85aced617419369d3faf
Ryan T. Claussen <rtclauss@us.ibm.com>
2018-06-26 13:12:33 -0500

    update java locations

--- src/main/java/com/ibm/hybrid/cloud/sample/HealthEndpoint.java
+++ src/main/java/com/ibm/hybrid/cloud/sample/HealthEndpoint.java
@@ -1,9 +1,8 @@
-package application;
+package com.ibm.hybrid.cloud.sample;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
